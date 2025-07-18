97c5843bf366e68901d5e637d0377ea34c231042
Aashutosh Tiwari <158829026+AASHU7688@users.noreply.github.com>
2025-07-18 23:18:06 +0530

    Add files via upload

---
+++ Loops/3_question.cpp
@@ -0,0 +1,12 @@
+// Reverse for loop. Print n to 1.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number: ";
+    cin>>n;
+    for(int i=n;i>=1;i--){
+        cout<<i<<endl;
+    }
+    return 0;
+}
