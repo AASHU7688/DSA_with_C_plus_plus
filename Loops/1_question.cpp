1e3a2d0f008f1b05ccb8b434387c9b2969fd86fc
Aashutosh Tiwari <158829026+AASHU7688@users.noreply.github.com>
2025-07-18 23:17:09 +0530

    Create 1_question.cpp

---
+++ Loops/1_question.cpp
@@ -0,0 +1,11 @@
+// Accept an integer and Print hello world n times
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    for(int i=1;i<=n; i++){
+        cout<<"hello world"<<endl;
+    }
+}
