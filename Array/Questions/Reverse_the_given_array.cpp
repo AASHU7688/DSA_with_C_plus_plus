01c6d48a813832537217404c9dd4afe8327df4e5
Aashutosh Tiwari <158829026+AASHU7688@users.noreply.github.com>
2025-08-09 20:48:00 +0530

    Create Reverse_the_given_array.cpp

---
+++ Array/Questions/Reverse_the_given_array.cpp
@@ -0,0 +1,30 @@
+// Reverse the given array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Reverse array
+    for (int i = 0, j = n - 1; i < j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
